80dca4bc6dea1d69b3bfd2e40c2b33d171e84f26
outis <mckinnondconnor+github@gmail.com>
2020-05-24 20:27:54 -0400

    add customization outlines to cms

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,6 @@
 import { Link } from "gatsby"
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class Footer extends React.Component {
   render() {
@@ -8,12 +9,32 @@ class Footer extends React.Component {
       <footer className="footer">
         <div className="container">
           <div className="content columns">
-            <div className="column has-text-centered">
+            <div className="column has-text-left">
               <ul className="footer-list is-marginless">
                 <li className="footer-list-item">
                   &copy; {currentYear > 2020 ? "2020 - " + currentYear : currentYear } Lauren Ebidia
                 </li>
               </ul>
+            </div>
+            <div className="column has-text-right">
+              <span class="icon has-text-primary">
+                <FontAwesomeIcon icon={['fab', 'linkedin']} />
+              </span>
+              <span class="icon has-text-primary">
+                <FontAwesomeIcon icon={['fab', 'facebook']} />
+              </span>
+              <span class="icon has-text-primary">
+                <FontAwesomeIcon icon={['fab', 'twitter']} />
+              </span>
+              <span class="icon has-text-primary">
+                <FontAwesomeIcon icon={['fab', 'github']} />
+              </span>
+              <span class="icon has-text-primary">
+                <FontAwesomeIcon icon={['fab', 'medium']} />
+              </span>
+              <span class="icon has-text-primary">
+                <FontAwesomeIcon icon={['fab', 'stack-overflow']} />
+              </span>
             </div>
           </div>
         </div>
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -8,9 +8,14 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
 import Header from "./header"
 import Footer from "./footer"
 import "../css/index.scss"
+
+library.add(fab)
 
 
 const Layout = ({ children }) => {
