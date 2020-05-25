1e7b9f0d29c4772f0628ca917567d3ecb3646afb
outis <mckinnondconnor+github@gmail.com>
2020-05-24 22:25:45 -0400

    use personal info for about page

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
+import { createMarkup } from "../utils/markup"
 
 export default function Template({ data }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
@@ -12,7 +13,7 @@ export default function Template({ data }) {
         <h1 className="main-title is-size-1 has-text-dark">{frontmatter.title}</h1>
         <div
           className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
+          dangerouslySetInnerHTML={createMarkup(html)}
         />
       </div>
     </Layout>
