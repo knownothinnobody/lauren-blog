008bff41c5482a14c7b306a8e30fe11f4dbb8a7a
outis <mckinnondconnor+github@gmail.com>
2020-05-25 22:43:57 -0400

    initial layout of contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -31,7 +31,44 @@ function ContactPage() {
       <SEO title="Contact" />
       <div className="container">
         <h1 className="main-title is-size-1">{frontmatter.contactTitle}</h1>
-        <div dangerouslySetInnerHTML={createMarkup(frontmatter.contactBody)}></div>
+        <div className="columns">
+          <div className="column">
+            <div dangerouslySetInnerHTML={createMarkup(frontmatter.contactBody)}></div>
+          </div>
+          <div className="column">
+            <form action="">
+              <div className="field">
+                <label className="label">Name</label>
+                <div className="control">
+                  <input className="input" type="text" placeholder="Text input" />
+                </div>
+              </div>
+
+              <div className="field">
+                <label className="label">Email</label>
+                <div className="control">
+                  <input className="input" type="email" placeholder="Email input" />
+                </div>
+              </div>
+
+              <div className="field">
+                <label className="label">Message</label>
+                <div className="control">
+                  <textarea className="textarea" placeholder="Message"></textarea>
+                </div>
+              </div>
+
+              <div className="field is-grouped">
+                <div className="control">
+                  <button className="button is-link">Submit</button>
+                </div>
+                <div className="control">
+                  <button className="button is-link is-light">Cancel</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </Layout>
   )
