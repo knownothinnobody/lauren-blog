77c140f927f566aa38e6f46a0c884ea15ac22cea
outis <mckinnondconnor+github@gmail.com>
2020-05-30 15:00:26 -0400

    update starter defaults

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,3 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
-
-// You can delete this file if you're not using it
-
 exports.onRenderBody = ({ setBodyAttributes }) => {
   setBodyAttributes({
     className: "has-navbar-fixed-top",
