4db824f55b6a3af0393f5297b034d2d586a70f83
outis <mckinnondconnor+github@gmail.com>
2020-05-30 15:08:40 -0400

    Fix Plugin Manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,11 +40,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `MyBlueBackpack`,
+        short_name: `bluebackpack`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#2F2FA2`,
+        theme_color: `#2F2FA2`,
         display: `minimal-ui`,
         icon: `src/images/logo-icon.png`, // This path is relative to the root of the site.
       },
