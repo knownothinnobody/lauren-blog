e62158239bbff6bc5636aafbad64e665d8492913
outis <mckinnondconnor+github@gmail.com>
2020-06-03 20:34:08 -0400

    add card tests

--- __tests__/components/card.js
+++ __tests__/components/card.js
@@ -7,4 +7,23 @@ describe("Card", () => {
   it("renders correctly", () => {
     expect(render(<Card />)).toMatchSnapshot()
   })
+
+  it("renders with props correctly", () => {
+    expect(
+      render(
+        <Card
+          title="Title"
+          content="<h1>TEST</h1>"
+          path="/blog/test-slug"
+          posterImage="/static/assets/logo.png"
+          date="May 22, 2020"
+          readingTime="5"
+        />
+      )
+    )
+  })
+
+  it("renders with additional classes correctly", () => {
+    expect(render(<Card additionalClasses="test-class" />))
+  })
 })
